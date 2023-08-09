65041bf1fae9cda54f8e7a658387a948aa12f759
bassant09 <bassantkhattab09@gmail.com>
2023-08-09 09:49:42 +0300

    update

--- src/app/contract/contract-routing.module.ts
+++ src/app/contract/contract-routing.module.ts
@@ -4,7 +4,8 @@ import { ContractComponent } from './contract.component';
 import { MainContractComponent } from './pages/main-contract/main-contract.component';
 
 const routes: Routes = [{ path: '', component: ContractComponent ,children:[
-  { path: 'contract', component: MainContractComponent}
+  { path: 'contract', component: MainContractComponent},
+  { path: '', redirectTo: 'contract', pathMatch: 'full' },
 ]}];
 
 @NgModule({
